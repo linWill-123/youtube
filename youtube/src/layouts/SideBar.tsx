e63996e6e216588bad99093bda152cdbf785b478
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-14 20:35:44 -0800

    added more buttons to side bar

--- youtube/src/layouts/SideBar.tsx
+++ youtube/src/layouts/SideBar.tsx
@@ -1,10 +1,19 @@
-import { Home } from "lucide-react";
+import { Clapperboard, Home, Library, Repeat } from "lucide-react";
 import { ElementType } from "react";
+import { buttonStyles } from "../components/Button";
+import { twMerge } from "tailwind-merge";
 
 export function SideBar() {
   return (
     <aside className="stick top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
       <SmallSideBarItem Icon={Home} title="Home" url="/" />
+      <SmallSideBarItem Icon={Repeat} title="shorts" url="/shorts" />
+      <SmallSideBarItem
+        Icon={Clapperboard}
+        title="Subscriptions"
+        url="/Subscriptions"
+      />
+      <SmallSideBarItem Icon={Library} title="Library" url="/Library" />
     </aside>
   );
 }
@@ -17,7 +26,13 @@ type SmallSideBarItemProps = {
 
 function SmallSideBarItem({ Icon, title, url }: SmallSideBarItemProps) {
   return (
-    <a href={url} className="">
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        "py-4 px-1 flex flex-col items-center rounded-lg gap-1"
+      )}
+    >
       <Icon className="w-6 h-6" />
       <div className="text-sm">{title}</div>
     </a>
